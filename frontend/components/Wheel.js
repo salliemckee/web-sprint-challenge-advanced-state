4d4be9fdecb8818adc3516262767457327c2138c
Sallie McKee <salliemckee@Morrigan.local>
2022-03-25 12:15:06 -0500

    setting up action creators

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { moveClockwise } from "../state/action-creators";
 
 function Wheel(props) {
   return (
--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,17 +1,63 @@
 // ❗ You don't need to add extra action creators to achieve MVP
-export function moveClockwise() { }
+import axios from "axios";
+import {
+  MOVE_CLOCKWISE,
+  MOVE_COUNTERCLOCKWISE,
+  SET_QUIZ_INTO_STATE,
+  SET_SELECTED_ANSWER,
+  SET_INFO_MESSAGE,
+  INPUT_CHANGE,
+  RESET_FORM,
+} from "./action-types";
 
-export function moveCounterClockwise() { }
+export function moveClockwise(clockwise) {
+  return {
+    type: MOVE_CLOCKWISE,
+    payload: clockwise,
+  };
+}
 
-export function selectAnswer() { }
+export function moveCounterClockwise(counterClockwise) {
+  return {
+    type: MOVE_COUNTERCLOCKWISE,
+    payload: counterClockwise,
+  };
+}
 
-export function setMessage() { }
+export function selectAnswer(answer) {
+  return {
+    type: SET_SELECTED_ANSWER,
+    payload: answer,
+  };
+}
 
-export function setQuiz() { }
+export function setMessage(message) {
+  return {
+    type: SET_INFO_MESSAGE,
+    payload: message,
+  };
+}
 
-export function inputChange() { }
+export function setQuiz(quiz) {
+  return {
+    type: SET_QUIZ_INTO_STATE,
+    payload: quiz,
+  };
+}
 
-export function resetForm() { }
+export function inputChange(change) {
+  return {
+    type: INPUT_CHANGE,
+    payload: change,
+  };
+}
+
+export function resetForm(reset) {
+  return {
+    type: RESET_FORM,
+    payload: reset,
+  };
+}
 
 // ❗ Async action creators
 export function fetchQuiz() {
@@ -19,7 +65,8 @@ export function fetchQuiz() {
     // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
     // On successful GET:
     // - Dispatch an action to send the obtained quiz to its state
-  }
+    // dispatch({type: RESET_FORM});
+  };
 }
 export function postAnswer() {
   return function (dispatch) {
@@ -27,13 +74,13 @@ export function postAnswer() {
     // - Dispatch an action to reset the selected answer state
     // - Dispatch an action to set the server message to state
     // - Dispatch the fetching of the next quiz
-  }
+  };
 }
 export function postQuiz() {
   return function (dispatch) {
     // On successful POST:
     // - Dispatch the correct message to the the appropriate state
     // - Dispatch the resetting of the form
-  }
+  };
 }
 // ❗ On promise rejections, use log statements or breakpoints, and put an appropriate error message in state
